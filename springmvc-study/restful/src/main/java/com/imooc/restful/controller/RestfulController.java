cff77eaab596cde00507955d70846c9cf5f98a5d
Zane47 <1228073191@qq.com>
2022-01-02 15:50:13 +0800

    路径变量和@RestController

--- springmvc-study/restful/src/main/java/com/imooc/restful/controller/RestfulController.java
+++ springmvc-study/restful/src/main/java/com/imooc/restful/controller/RestfulController.java
@@ -3,33 +3,34 @@ package com.imooc.restful.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("restful")
 public class RestfulController {
 
     @GetMapping("/request")
-    @ResponseBody
+    // @ResponseBody
     public String doGetRequest() {
 
         //使用\原义输出
         return "{\"message\":\"返回查询结果\"}";
     }
 
-
-    @PostMapping("/request")
-    @ResponseBody
-    public String doPostRequest() {
-        return "{\"message\":\"数据新建成功\"}";
+    // POST /article/1
+    // POST /restful/request/100
+    @PostMapping("/request/{rid}")
+    // @ResponseBody
+    public String doPostRequest(@PathVariable("rid") Integer requestId) {
+        return "{\"message\":\"数据新建成功\", \"id\": "+requestId+"}";
     }
 
     @PutMapping("/request")
-    @ResponseBody
+    // @ResponseBody
     public String doPutRequest() {
         return "{\"message\":\"数据更新成功\"}";
     }
 
     @DeleteMapping("/request")
-    @ResponseBody
+    // @ResponseBody
     public String doDeleteRequest() {
         return "{\"message\":\"数据删除成功\"}";
     }
