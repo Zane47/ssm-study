5379afc44a1d61be2c5a3855e76e0787972f0520
Zane47 <1228073191@qq.com>
2022-01-02 15:31:14 +0800

    不同type的请求

--- springmvc-study/restful/src/main/java/com/imooc/restful/controller/RestfulController.java
+++ springmvc-study/restful/src/main/java/com/imooc/restful/controller/RestfulController.java
@@ -1,9 +1,7 @@
 package com.imooc.restful.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("restful")
@@ -17,5 +15,23 @@ public class RestfulController {
         return "{\"message\":\"返回查询结果\"}";
     }
 
+
+    @PostMapping("/request")
+    @ResponseBody
+    public String doPostRequest() {
+        return "{\"message\":\"数据新建成功\"}";
+    }
+
+    @PutMapping("/request")
+    @ResponseBody
+    public String doPutRequest() {
+        return "{\"message\":\"数据更新成功\"}";
+    }
+
+    @DeleteMapping("/request")
+    @ResponseBody
+    public String doDeleteRequest() {
+        return "{\"message\":\"数据删除成功\"}";
+    }
 
 }
